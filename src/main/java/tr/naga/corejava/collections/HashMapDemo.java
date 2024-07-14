cf5dc77b5aed1ef48f93f9bfbcd097c9065ab7a3
Naga-java <nagarjuna.java3@gmail.com>
2024-07-14 22:09:38 +0530

    NN

---
+++ src/main/java/tr/naga/corejava/collections/HashMapDemo.java
@@ -0,0 +1,30 @@
+package tr.naga.corejava.collections;
+
+import java.util.*;
+
+class HashMapDemo{
+
+public static void main(String[] args){
+
+
+HashMap hm=new HashMap();
+
+hm.put(1,"Naga");
+
+hm.put(2,"arjun");
+
+hm.put(3,"arun");
+
+hm.put(3,"Nagarjuna");
+
+hm.put(3,"kishore");
+
+hm.put(2,"sunny");
+
+System.out.println(hm);
+
+
+
+}
+
+}
